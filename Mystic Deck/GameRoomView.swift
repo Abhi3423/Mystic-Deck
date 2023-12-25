7a4babc1d3dc01f63aeaa64f1b103002c8f954e0
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-12-25 17:07:24 +0530

    cards design created

---
+++ Mystic Deck/GameRoomView.swift
@@ -0,0 +1,18 @@
+//
+//  GameRoomView.swift
+//  Mystic Deck
+//
+//  Created by user1 on 24/12/23.
+//
+
+import SwiftUI
+
+struct GameRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameRoomView()
+}
---
+++ UI Components/Loading.swift
@@ -0,0 +1,18 @@
+//
+//  Loading.swift
+//  Mystic Deck
+//
+//  Created by user1 on 24/12/23.
+//
+
+import SwiftUI
+
+struct Loading: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loading()
+}
