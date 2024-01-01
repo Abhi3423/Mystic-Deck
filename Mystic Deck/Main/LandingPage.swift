7785ff59e511f1e854c80f7e4d4b0f7853919872
Hitarth00011 <hn7323@srmist.edu.in>
2024-01-01 20:43:46 +0530

    added landingpage and waiting room

---
+++ Mystic Deck/Main/LandingPage.swift
@@ -0,0 +1,8 @@
+//
+//  LandingPage.swift
+//  Mystic Deck
+//
+//  Created by Hitarth Rathod on 01/01/24.
+//
+
+import Foundation
---
+++ Mystic Deck/Main/WaitingRoom.swift
@@ -0,0 +1,8 @@
+//
+//  WaitingRoom.swift
+//  Mystic Deck
+//
+//  Created by Hitarth Rathod on 01/01/24.
+//
+
+import Foundation
